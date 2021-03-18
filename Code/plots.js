12aa435b7f3c2d90210bee70bebb83c0b9693483
unknown <d.jahnsen.bes@gmail.com>
2021-03-18 16:35:46 -0400

    Bubble Chart

--- Code/plots.js
+++ Code/plots.js
@@ -1,39 +1,293 @@
-// poke_url = "https://pokeapi.co/api/v2/stat/"
-// var pokemon_name = d3.select("#poke1").property("value");
-// console.log(poke_name)
-// function buildPlot(name) {
-//     var url=`https://pokeapi.co/api/v2/pokemon/${name}`;
-//     d3.json(url).then(function(data) {
-//       // Grab values from the response json object to build the plots
-//       var name = data.forms[0].name;
-//       var attack = data.stats[1].base_stat;
-//       var defense = data.stats[2].base_stat;
-//       var special_attack = data.stats[3].base_stat;
-//       var special_defense = data.stats[4].base_stat;
-//       var speed = data.stats[5].base_stat;
-
-//       console.log(`Name: ${name}, attack: ${attack}, defense:${defense}, special_attack:${special_attack}`);
-//     })
-//   };
-// buildPlot(poke_name);
+//Create a function to build the bubble
 
 function bubble_chart(gen){
-    var url ="https://pokeapi.co/api/v2/generation/";
-    d3.json(url+gen).then((data)=>{
+    //Get the data from the csv
+    d3.csv("poke_data.csv").then(function(data){
+    
+        //Create arrays for the different types
+        var normal = [];
+        var water = [];
+        var elec = [];
+        var fight = [];
+        var ground = [];
+        var psy = [];
+        var rock = [];
+        var dark = [];
+        var steel = [];
+        var fire = [];
+        var grass = [];
+        var ice = [];
+        var poison = [];
+        var fly = [];
+        var bug = [];
+        var ghost = [];
+        var dragon = [];
+        var fairy = [];
 
-        //Store the values of the data to use
-        pokemon = data.pokemon_species;
+        //Loop through the array and append the pokemon to the type list
+        for (i = 0; i< data.length; i++){
+            //Checks the generation for each pokemon
+            if (data[i].Generation == gen){
+                //Long conditional to check each type and add to the list for the first type
+                if (data[i].Type1 == "Normal" || data[i].Type2 == "Normal"){
+                    normal.push(data[i]);
+                } else if (data[i].Type1 == "Water"){
+                    water.push(data[i]);
+                } else if (data[i].Type1 == "Electric"){
+                    elec.push(data[i]);
+                } else if (data[i].Type1 == "Fighting"){
+                    fight.push(data[i]);
+                } else if (data[i].Type1 == "Ground"){
+                    ground.push(data[i]);
+                } else if (data[i].Type1 == "Psychic"){
+                    psy.push(data[i]);
+                } else if (data[i].Type1 == "Rock"){
+                    rock.push(data[i]);
+                } else if (data[i].Type1 == "Dark"){
+                    dark.push(data[i]);
+                } else if (data[i].Type1 == "Steel"){
+                    steel.push(data[i]);
+                } else if (data[i].Type1 == "Fire"){
+                    fire.push(data[i]);
+                } else if (data[i].Type1 == "Grass"){
+                    grass.push(data[i]);
+                } else if (data[i].Type1 == "Ice"){
+                    ice.push(data[i]);
+                } else if (data[i].Type1 == "Poison"){
+                    poison.push(data[i]);
+                } else if (data[i].Type1 == "Flying"){
+                    fly.push(data[i]);
+                } else if (data[i].Type1 == "Bug"){
+                    bug.push(data[i]);
+                } else if (data[i].Type1 == "Ghost"){
+                    ghost.push(data[i]);
+                } else if (data[i].Type1 == "Dragon"){
+                    dragon.push(data[i]);
+                } else if (data[i].Type1 == "Fairy"){
+                    fairy.push(data[i]);
+                } 
+                else{
+                    console.log(`Could not place ${data[i].Name} with a type`)
+                }
 
-        //Create arrays to hold the types
-        var type1 = [];
-        var type2 = [];
+                //Repeat the loop for type 2
+                if (data[i].Type2 == "Normal"){
+                    normal.push(data[i]);
+                } else if (data[i].Type2 == "Water"){
+                    water.push(data[i]);
+                } else if (data[i].Type2 == "Electric"){
+                    elec.push(data[i]);
+                } else if (data[i].Type2 == "Fighting"){
+                    fight.push(data[i]);
+                } else if (data[i].Type2 == "Ground"){
+                    ground.push(data[i]);
+                } else if (data[i].Type2 == "Psychic"){
+                    psy.push(data[i]);
+                } else if (data[i].Type2 == "Rock"){
+                    rock.push(data[i]);
+                } else if (data[i].Type2 == "Dark"){
+                    dark.push(data[i]);
+                } else if (data[i].Type2 == "Steel"){
+                    steel.push(data[i]);
+                } else if (data[i].Type2 == "Fire"){
+                    fire.push(data[i]);
+                } else if (data[i].Type2 == "Grass"){
+                    grass.push(data[i]);
+                } else if (data[i].Type2 == "Ice"){
+                    ice.push(data[i]);
+                } else if (data[i].Type2 == "Poison"){
+                    poison.push(data[i]);
+                } else if (data[i].Type2 == "Flying"){
+                    fly.push(data[i]);
+                } else if (data[i].Type2 == "Bug"){
+                    bug.push(data[i]);
+                } else if (data[i].Type2 == "Ghost"){
+                    ghost.push(data[i]);
+                } else if (data[i].Type2 == "Dragon"){
+                    dragon.push(data[i]);
+                } else if (data[i].Type2 == "Fairy"){
+                    fairy.push(data[i]);
+                } 
+                else{
+                    // console.log(`Could not place ${data[i].Name} with a type`)
+                }
+            } else if (gen == "All"){
+                //Assumes the all generation is selected
+                //Repeats the process but for all generations
+                if (data[i].Type1 == "Normal" || data[i].Type2 == "Normal"){
+                    normal.push(data[i]);
+                } else if (data[i].Type1 == "Water"){
+                    water.push(data[i]);
+                } else if (data[i].Type1 == "Electric"){
+                    elec.push(data[i]);
+                } else if (data[i].Type1 == "Fighting"){
+                    fight.push(data[i]);
+                } else if (data[i].Type1 == "Ground"){
+                    ground.push(data[i]);
+                } else if (data[i].Type1 == "Psychic"){
+                    psy.push(data[i]);
+                } else if (data[i].Type1 == "Rock"){
+                    rock.push(data[i]);
+                } else if (data[i].Type1 == "Dark"){
+                    dark.push(data[i]);
+                } else if (data[i].Type1 == "Steel"){
+                    steel.push(data[i]);
+                } else if (data[i].Type1 == "Fire"){
+                    fire.push(data[i]);
+                } else if (data[i].Type1 == "Grass"){
+                    grass.push(data[i]);
+                } else if (data[i].Type1 == "Ice"){
+                    ice.push(data[i]);
+                } else if (data[i].Type1 == "Poison"){
+                    poison.push(data[i]);
+                } else if (data[i].Type1 == "Flying"){
+                    fly.push(data[i]);
+                } else if (data[i].Type1 == "Bug"){
+                    bug.push(data[i]);
+                } else if (data[i].Type1 == "Ghost"){
+                    ghost.push(data[i]);
+                } else if (data[i].Type1 == "Dragon"){
+                    dragon.push(data[i]);
+                } else if (data[i].Type1 == "Fairy"){
+                    fairy.push(data[i]);
+                } 
+                else{
+                    console.log(`Could not place ${data[i].Name} with a type`)
+                }
 
-        //Loop through the pokemon array, call the json and store the types
-        for (var i = 0; i < pokemon.length; i++){
+                //Repeat the loop for type 2
+                if (data[i].Type2 == "Normal"){
+                    normal.push(data[i]);
+                } else if (data[i].Type2 == "Water"){
+                    water.push(data[i]);
+                } else if (data[i].Type2 == "Electric"){
+                    elec.push(data[i]);
+                } else if (data[i].Type2 == "Fighting"){
+                    fight.push(data[i]);
+                } else if (data[i].Type2 == "Ground"){
+                    ground.push(data[i]);
+                } else if (data[i].Type2 == "Psychic"){
+                    psy.push(data[i]);
+                } else if (data[i].Type2 == "Rock"){
+                    rock.push(data[i]);
+                } else if (data[i].Type2 == "Dark"){
+                    dark.push(data[i]);
+                } else if (data[i].Type2 == "Steel"){
+                    steel.push(data[i]);
+                } else if (data[i].Type2 == "Fire"){
+                    fire.push(data[i]);
+                } else if (data[i].Type2 == "Grass"){
+                    grass.push(data[i]);
+                } else if (data[i].Type2 == "Ice"){
+                    ice.push(data[i]);
+                } else if (data[i].Type2 == "Poison"){
+                    poison.push(data[i]);
+                } else if (data[i].Type2 == "Flying"){
+                    fly.push(data[i]);
+                } else if (data[i].Type2 == "Bug"){
+                    bug.push(data[i]);
+                } else if (data[i].Type2 == "Ghost"){
+                    ghost.push(data[i]);
+                } else if (data[i].Type2 == "Dragon"){
+                    dragon.push(data[i]);
+                } else if (data[i].Type2 == "Fairy"){
+                    fairy.push(data[i]);
+                } 
+                else{
+                    // console.log(`Could not place ${data[i].Name} with a type`)
+                }
+            }
+    
+        };
 
+        //Create the datasets for the bubble chart
+        var x_values = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800];
+        var y_values = [
+            normal.length,
+            water.length,
+            elec.length,
+            fight.length,
+            ground.length,
+            psy.length,
+            rock.length,
+            dark.length,
+            steel.length,
+            fire.length,
+            grass.length,
+            ice.length,
+            poison.length,
+            fly.length,
+            bug.length,
+            ghost.length,
+            dragon.length,
+            fairy.length
+        ];
+        var labels = ["Normal", "Water", "Electric", "Fighting", "Ground", "Psychic", "Rock",
+            "Dark", "Steel", "Fire", "Grass", "Ice", "Poison", "Fly", "Bug", "Ghost", "Dragon", "Fairy"
+        ];
+
+        //Console log the number of pokemon
+        var pokemon = 0;
+        for (i=0; i<y_values.length; i++){
+            pokemon += y_values[i]
         }
+        console.log(pokemon)
+        
+        // Create the chart data
+        var bubble_values = [
+            {
+                x: labels,
+                y: y_values,
+                text: labels,
+                mode: "markers",
+                marker: {
+                    size: y_values,
+                    color: x_values,
+                    colorscale: "Jet"
+                }
+            }
+        ];
 
+        //Create the chart layout
+        var layout = {
+            title: `Types of Pokemon for Generation: ${gen}`,
+            margin: { t:0},
+            hovermode: "closest",
+            xaxis: {title: "Pokemon Type"},
+            margin: { t: 30}
+        };
+        
+        //Plot the chart
+        Plotly.newPlot("bubble", bubble_values, layout);
     });
 };
 
-bubble_chart("1");
+//Create an init function 
+function init(){
+    //Create the array of selections
+    var choices = ["All", "1", "2", "3", "4", "5", "6"];
+    
+    //Grab a reference to the doprdown select element
+    var selector = d3.select("#selDataset");
+
+    // Populate the selector with the choices
+    choices.forEach((choice)=>{
+        selector
+            .append("option")
+            .text(choice)
+            .property("value", choice);
+    });
+
+    //Build the first plots for all generation
+    bubble_chart(choices[0]);
+
+}
+
+//Creat an option changed function
+function optionChanged(newChoice){
+    //Create the new charts
+    bubble_chart(newChoice);
+}
+
+//Initalize the dashboard
+init();

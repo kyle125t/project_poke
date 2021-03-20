5356d55ccefd352f88a8473fb6fd8a9209bae213
Kyle Terramoccia <kyleterramoccia@kyles-mbp.myfiosgateway.com>
2021-03-20 13:08:31 -0400

    Updated Types files

--- Code/appTypes.js
+++ Code/appTypes.js
@@ -1,44 +1,89 @@
-// const Pokedex = require("pokeapi-js-wrapper")
-// const customOptions = {
-//   protocol: "https",
-//   hostName: "pokeapi.co",
-//   versionPath: "/api/v2/",
-//   cache: true,
-//   timeout: 5 * 1000, // 5s
-//   cacheImages: true
-// }
-// const P = new Pokedex.Pokedex(customOptions)
-
-// P.getTypeByName("pikachu").then(function(response) {
-//     console.log(response)
-//   })
-
-
-
 function pokeSubmit(){
     var param = document.getElementById("pokeInput").value;
-    var pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param;
+    var pokeURL = "http://pokeapi.co/api/v2/pokemon/" + param.toLowerCase();
+    var jsonType1;
+    var jsonType2;
 
     $.getJSON(pokeURL, function(data){
-        //console.log(data);
-        //console.log(JSON.stringify(data, null, "  "));
-
         var pokeID = data.id;
         var pokeName = data.name;
+        var imageURI = data.sprites.front_default;
         var pokeType1 = data.types[0].type.name;
+        var type1URL = "https://pokeapi.co/api/v2/type/" + pokeType1 + "/";
         if (data.types.length == 2) {
             var pokeType2 = data.types[1].type.name;
         }
         else var pokeType2 = null;
-        
+        $.getJSON(type1URL, function(data2){
+            var doubleFrom = data2.damage_relations.double_damage_from;
+            var doubleTo = data2.damage_relations.double_damage_to;
+            var halfFrom = data2.damage_relations.half_damage_from;
+            var halfTo = data2.damage_relations.half_damage_to;
+            var noFrom = data2.damage_relations.no_damage_from;
+            var noTo = data2.damage_relations.no_damage_to;
+
+            // console.log(doubleFrom);
+            // console.log(doubleTo);
+            // console.log(halfFrom);
+            // console.log(halfTo);
+            // console.log(noFrom);
+            // console.log(noTo);
+            
+            var dF = [];
+            var dT = [];
+            var hF = [];
+            var hT = [];
+            var nF = [];
+            var nT = [];
+            
+            var halfWeak
+            var doubleWeak
+            var halfStrong
+            var doubleStrong
+
+            // console.log(doubleFrom);
+
+            doubleFrom.forEach(function(data){
+                var x = data.name;
+                dF.push(x);
+            })
+            // console.log(dF);
+            doubleTo.forEach(function(data){
+                var x = data.name;
+                dT.push(x);
+            })
+            halfFrom.forEach(function(data){
+                var x = data.name;
+                hF.push(x);
+            })
+            halfTo.forEach(function(data){
+                var x = data.name;
+                hT.push(x);
+            })
+            noFrom.forEach(function(data){
+                var x = data.name;
+                nF.push(x);
+            })
+            noTo.forEach(function(data){
+                var x = data.name;
+                nT.push(x);
+            })
+
+        });
+
+
         console.log("Number: ", pokeID);
         console.log("Name: ", pokeName);
         console.log("Type 1: ", pokeType1);
         console.log("Type 2: ", pokeType2);
+        console.log("Image URI: ", imageURI);
+        // console.log("Double From: ", doubleF);
 
         var li = "";
-            li += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
+            li += '<li><img src="' + imageURI + '">';
+            li += '<h1>#' + pokeID + ' ' + pokeName.toUpperCase() + '</h1>';
             li += '<p>Type 1: ' + pokeType1.toUpperCase() + '</p>';
+
             // only display Type 2 if it is not null
             if (pokeType2 != null){
                 li += '<p>Type 2: ' + pokeType2.toUpperCase() + '</p>';
@@ -52,70 +97,5 @@ function pokeSubmit(){
             //     function(){
             //         $(this).listview("refresh");
             // });
-
-    })
-};
-
-// function pokeSubmit(){
-//     var param = document.getElementById("pokeInput").value;
-//     var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
-//     var pokeURL2 = "http://pokeapi.co/api/v2/pokemon/" + param;
-
-//     $.getJSON(pokeURL, function(data){
-//         //console.log(data);
-//         var pokeID = data.national_id;
-//         var pokeName = data.name;
-//         var pokeType1 = data.types[0].name;
-//         if (data.types.length == 2) {
-//             var pokeType2 = data.types[1].name;
-//         }
-//         else var pokeType2 = null;
-//         var descriptionURI = "http://pokeapi.co" + data.descriptions[0].resource_uri;
-//         var pokeDescription = "";
-
-//         $.getJSON(descriptionURI, function(data2){
-//             //console.log(data2);
-//             pokeDescription = data2.description;
-//         });
-
-//         $.getJSON(pokeURL2, function(data3){
-//             //console.log(data3);
-
-//              //console.log(JSON.stringify(data, null, "  "));
-//             var imageURI = data3.sprites.front_default;
-
-//             console.log("Number: ", pokeID);
-//             console.log("Name: ", pokeName);
-//             console.log("Type 1: ", pokeType1);
-//             console.log("Type 2: ", pokeType2);
-//             console.log("Description URI: ", descriptionURI);
-//             console.log("Description: ", pokeDescription);
-//             console.log("Image URI: ", imageURI);
-
-//             // append data to HTML
-//             // empty string to hold HTML
-//             var li = "";
-//             li += '<li><img src="' + imageURI + '">';
-//             li += '<h1>#' + pokeID + ' ' + pokeName + '</h1>';
-//             li += '<p>Type 1: ' + pokeType1 + '</p>';
-
-//             // only display Type 2 if it is not null
-//             if (pokeType2 != null){
-//                 li += '<p>Type 2: ' + pokeType2 + '</p>';
-//             }
-
-//             li += '<p>' + pokeDescription + '</p>';
-//             li += '</li>';
-
-//             // empty the listview
-//             $("#pokeDetails").empty();
-
-//             // append new li to listview
-//             $("#pokeDetails").append(li).promise().done(function(){
-//                     $(this).listview("refresh");
-//             });
-
-//         });
-
-//     });
-// };
+        });
+    };
